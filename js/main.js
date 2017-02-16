139dcfb3f6ba726af3c8bc45fc6212b9e2b6f706
Josh Diaz <sagetopia@Joshs-iMac.local>
2017-02-16 16:51:03 -0500

    add magnific popup

--- js/main.js
+++ js/main.js
@@ -11,6 +11,12 @@
   };
 }(jQuery));
 
+(function($) {
+  $(document).ready(function() {
+    $('.image-popup').magnificPopup({type:'image'});
+  });
+}(jQuery));
+
 function updateViewportDimensions() {
 	var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
 	return { width:x,height:y };
